9f1745ef4c85457626026d03be12dd605277107a
OPiastri81 <stardustdata@gmail.com>
2025-07-06 18:56:44 +0900

    応用数学研究

--- cluster_analysis/file1.R
+++ cluster_analysis/file1.R
@@ -12,3 +12,16 @@ hc <- hclust(d, method = "ward.D2") #
 # hang = -1は見やすくするために枝の高さを揃えるオプション
 # cex = 0.6は文字サイズを調整するオプション
 plot(hc, hang = -1, cex = 0.6, main = "Dendrogram of USArrests") #
+
+# (b)で実行したクラスタリング結果(hc)を4つのクラスターに分割
+clusters <- cutree(hc, k = 4) # 
+
+# 分割結果の確認
+print(clusters)
+
+# (参考)クラスター番号ごとに州の名前を一覧表示するプログラム
+ for (i in 1:4) {
+   cat("Cluster", i, ":\n")
+   print(names(clusters[clusters == i]))
+   cat("\n")
+ }
